6be77c61b93d19ea9df95b2b599482b542558f0a
areej <58537880+ughareej@users.noreply.github.com>
2022-02-19 16:04:12 -0500

    Update edge.py

--- edge.py
+++ edge.py
@@ -1,81 +1,216 @@
 import cv2
-import math
 import numpy as np
+from math import ceil, pi, tan, e, atan
 
-img = cv2.imread("cat2.jpg")
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+DATATYPE = np.uint8
 
-cv2.imshow("gray cat (original) ",gray)
+# Returns an array of stick kernels.
+# It can have more tiles in the stick than the size, but it preserves the same behaviour.
+def makeStickKernels(size,stick_count):
+    angle_step = 2 * pi / stick_count
+    kernels = []
 
-def myEdgeFilter(image, sigma):
-    hsize = 2 * math.ceil(3 * sigma) + 1  #dimension of kernel
-    h2 = hsize//2
-    kernel = [[0] * hsize] * hsize
+    for step in range(stick_count):
+        angle = step * angle_step
+        
+        kernel = [[0 for i in range(size)] for j in range(size)]
 
-    #making the kernel
-    for x in range(-h2, h2+1):
-        for y in range(-h2, h2+1):
-            e1 = 1/2*math.pi*(sigma**2)
-            e2 = math.exp(-(x**2 + y**2)/2*sigma**2)
-            kernel[x+h2][y+h2] = e1 * e2
+        slope = tan(angle)
+        intercept = (size / 2) * (1 - slope)
 
-    kernel = np.array(kernel)
+        if (abs(slope) < size):
+            x_intercepts = [slope * x + intercept for x in range(size)]
+            for x in range(size):
+                pos = x_intercepts[x]
+                if pos < size and pos >= 0:
+                    kernel[x][int(pos)] = 1
+                    if (x > 0):
+                        kernel[x - 1][int(pos)] = 1
+
+        if (abs(slope) > 1 / size):
+            y_intercepts = [(y - intercept) / slope for y in range(size)]
+            for y in range(size):
+                pos = y_intercepts[y]
+                if pos < size and pos >= 0:
+                    kernel[int(pos)][y] = 1
+                    if (y > 0):
+                        kernel[int(pos)][y - 1] = 1
+
+        kernels.append(kernel)
     
-    #convoluting it
-    m, n = image.shape  #dimensions of the original image
-    m = m - 1
-    n = n - 1
-    result = np.zeros((((m-hsize)+1),((n-hsize)+1)))
-
-    for i in range(0, (m-hsize)+1):  #0 - 361
-        for j in range(0, (n-hsize)+1): #0 - 410
-            box_vals = image[i : hsize+i, j: hsize+j]
-            newBox = box_vals * kernel
-            boxSum = 0.0
-            for b in range (0,hsize):
-                for c in range (0,hsize):
-                    boxSum = boxSum + newBox[b][c]
-            boxSum = boxSum / hsize**2
-            result[i][j] = boxSum
-                    
-    result.astype(np.uint8)
-    m, n = result.shape
-    m = m - 1
-    n = n - 1
-
-    # Sobel filter
-    sobelx = np.array([[-1, 0, 1],[-2, 0 ,2],[-1, 0, 1]])
-    sobely = np.array([[-1, -2, -1],[0, 0 ,0],[1, 2, 1]])
-    ssize = 3
-
-    # Convoluting with filter
-    x_image = np.zeros((((m-hsize)+1),((n-hsize)+1)))
-    y_image = np.zeros((((m-hsize)+1),((n-hsize)+1)))
+    return kernels
     
-    for i in range(0, (m-ssize)+1):  #0 - 351
-        for j in range(0, (n-ssize)+1): #0 - 400
-            box_vals2 = result[i : ssize+i, j: ssize+j]
-            XBox = box_vals2 * sobelx
-            YBox = box_vals2 * sobely
-            boxSumx = 0.0
-            boxSumy = 0.0
-            for b in range (0,ssize):
-                for c in range (0,ssize):
-                    boxSumx = boxSumx + XBox[b][c]
-                    boxSumy = boxSumy + YBox[b][c]
+# Applies the stick filter
+def stickFilter(img0,stick_count,size):
+    kernels = makeStickKernels(size,stick_count)
 
-# this is commented out because i get an error
-            # x_image[i][j] = boxSumx 
-            # y_image[i][j] = boxSumy
-            # f_image = boxSumy * boxSumx
+    shape = img0.shape
+    filteredImage = np.zeros((shape[0] - size + 2,shape[1] - size + 2),DATATYPE) 
 
+    for y in range(size // 2 + 1,shape[1] - size // 2 - 1):
+        for x in range(size // 2 + 1,shape[0] - size // 2 - 1):
+            max_delta = 0
+            for kernel in kernels:
+                average = 0
+                stick_average = 0
+                for conv_y in range(size):
+                    for conv_x in range(size):
+                        image_x = x - (size // 2) + conv_x
+                        image_y = y - (size // 2) + conv_y
+                        value = img0[image_x,image_y]
+                        if (kernel[conv_x][conv_y]):
+                            stick_average += value
+                        else:
+                            average += value
+                average /= size * size
+                stick_average /= sum([sum(row) for row in kernel])
+                delta = average - stick_average
+                if (delta > max_delta):
+                    max_delta = delta
+            filteredImage[x - size // 2 + 1][y - size // 2 + 1] = int(max_delta)
+    return filteredImage
 
+# A basic convolution function for 2d kernels
+def simpleConvolve(img,kernel):
+    shape = img.shape
+    kernel_shape = kernel.shape
+    filteredImage = np.zeros((shape[0] - kernel_shape[0] + 1,shape[1] - kernel_shape[1] + 1),DATATYPE) 
+    for y in range((kernel_shape[1] - 1) // 2,shape[1] - (kernel_shape[1] - 1) // 2):
+        for x in range((kernel_shape[0] - 1) // 2,shape[0] - (kernel_shape[0] - 1) // 2):
+            out_x = x - (kernel_shape[0] - 1) // 2
+            out_y = y - (kernel_shape[1] - 1) // 2
+            accumulator = 0
+            for conv_x in range(kernel_shape[0]):
+                for conv_y in range(kernel_shape[1]):
+                    image_x = x - (kernel_shape[0] // 2) + conv_x
+                    image_y = y - (kernel_shape[1] // 2) + conv_y
+                    if (image_x >= 0 and image_y >= 0 and image_x < shape[0] and image_y < shape[1]):
+                        accumulator += img[image_x][image_y] * kernel[conv_x][conv_y]
+            filteredImage[out_x][out_y] = int(min(255,max(accumulator,0)))
+    return filteredImage
 
-    cv2.imshow("blur cat",result)
-    # cv2.imshow("sobel cat",f_image)
+# Does a separated convolution using a gaussian filter
+def gaussianConvolve(img, sigma):
+    size = 2 * ceil(3 * sigma) + 1
+    kernel = np.zeros((size,1))
+    low  = -(size - 1) // 2
+    high = (size - 1) // 2 + 1
+    for i in range(low,high):
+        kernel[i + (size - 1) // 2][0] = (1 / (((2 * pi) ** 0.5) * sigma)) * (e ** -((i * i) / ( 2 * sigma * sigma)))
+    test = simpleConvolve(img,kernel)
+    img2 = simpleConvolve(test,np.transpose(kernel))
+    return img2
 
+def gradientMagnitudeThreshholding(image):
+    new_image = np.zeros((image.shape[0] - 2, image.shape[1] - 2),dtype=DATATYPE)
+
+    # The magnitudes as pixel intensities.
+    magnitude_image = np.zeros((image.shape[0] - 2, image.shape[1] - 2),dtype=np.float32)
     
+    for y in range(1,image.shape[1] - 1):
+        for x in range(1,image.shape[0] - 1):
+            ix = int(-image[x][y]) + int(image[x + 1][y])
+            iy = int(-image[x][y]) + int(image[x][y + 1])
+            magnitude = (ix * ix + iy * iy) ** 0.5
+            if (magnitude < 20):
+                new_image[x - 1][y - 1] = 0
+            else:
+                new_image[x - 1][y - 1] = image[x][y]
+            magnitude_image[x - 1][y - 1] = magnitude
+    return new_image, magnitude_image
 
-myEdgeFilter(gray, 1)
-cv2.waitKey(50000) #for some reason waitKey(0) doesnt work, so i set the program to end in 20 seconds
-cv2.destroyAllWindows()
+def nonMaximumSupression(image):
+    new_image = np.zeros((image.shape[0] - 2, image.shape[1] - 2),dtype=DATATYPE)
+
+    orientation_image = np.zeros((image.shape[0] - 2, image.shape[1] - 2),dtype=DATATYPE)
+    
+    for y in range(1,image.shape[1] - 1):
+        for x in range(1,image.shape[0] - 1):
+
+            # Approximation for direction
+            ix = int(-image[x][y]) + int(image[x + 1][y])
+            iy = int(-image[x][y]) + int(image[x][y + 1])
+            if (ix == 0):
+                direction = pi / 2
+            else:
+                direction = atan(iy/ix)
+
+            orientation_image[x - 1][y - 1] = min(max(int(((direction + pi / 2) / pi) * 255),0),255)
+
+            # Get closest angle and see if pixels on that angle are greater
+            if (direction > 3 * pi / 8 or direction < - 3 * pi / 8):
+                if (image[x][y] >= image[x][y - 1] and image[x][y] >= image[x][y + 1]):
+                    new_image[x - 1][y - 1] = image[x][y]
+                else:
+                    new_image[x - 1][y - 1] = 0
+            elif (direction > pi / 8):
+                if (image[x][y] >= image[x + 1][y + 1] and image[x][y] >= image[x - 1][y - 1]):
+                    new_image[x - 1][y - 1] = image[x][y]
+                else:
+                    new_image[x - 1][y - 1] = 0
+            elif (direction < -pi / 8):
+                if (image[x][y] >= image[x + 1][y - 1] and image[x][y] >= image[x - 1][y + 1]):
+                    new_image[x - 1][y - 1] = image[x][y]
+                else:
+                    new_image[x - 1][y - 1] = 0
+            else:
+                if (image[x][y] >= image[x + 1][y] and image[x][y] >= image[x - 1][y]):
+                    new_image[x - 1][y - 1] = image[x][y]
+                else:
+                    new_image[x - 1][y - 1] = 0
+    return new_image, orientation_image
+            
+        
+# Main function. Chains the other function to get the final result
+def myEdgeFilter(img0,sigma):
+    blurred = gaussianConvolve(img0, sigma)
+    print("Blurring complete.")
+
+    # Sobel Edge detection
+    # I included the reverse directions as well since it wasn't detecting very well without
+    kernel_nx = np.array([[-1,0,1],[-2,0,2],[-1,0,1]])
+    sobel_nx = simpleConvolve(blurred, kernel_nx)
+    print("Negative X Sobel filter complete.")
+
+    kernel_px = np.array([[1,0,-1],[2,0,-2],[-1,0,1]])
+    sobel_px = simpleConvolve(blurred, kernel_px)
+    print("Positive X Sobel filter complete.")
+
+    kernel_ny = np.array([[-1,-2,-1],[0,0,0],[1,2,1]])
+    sobel_ny = simpleConvolve(blurred, kernel_ny)
+    print("Negative Y Sobel filter complete.")
+
+    kernel_py = np.array([[1,2,1],[0,0,0],[-1,-2,-1]])
+    sobel_py = simpleConvolve(blurred, kernel_py)
+    print("Positive Y Sobel filter complete.")
+
+    # Combines all of the sobel edge detections into one.
+    combined = sobel_nx + sobel_px + sobel_ny + sobel_py
+    print("Combining edge filtered images complete.")
+
+    # Cuts off areas with minimal magnitude in the gradient
+    magnitude_threshold, magnitude_image = gradientMagnitudeThreshholding(combined)
+    print("Magnitude threshholding complete.")
+
+    # Applying stick filter
+    sticks_filtered = stickFilter(magnitude_threshold,5,8)
+    print("Sticks filter applied")
+
+    # Suppressing maximums
+    final_sticks, _ = nonMaximumSupression(sticks_filtered)
+    print("Supression complete.")
+
+    final_no_sticks, orientation_image = nonMaximumSupression(magnitude_threshold)
+    print("Supression complete.")
+    
+    return final_no_sticks, final_sticks, magnitude_image, orientation_image, combined
+
+# Calling the above functions
+file = "cat2.jpg"
+image = cv2.imread(file,0)
+no_sticks, sticks, magnitude, orientation,edges = myEdgeFilter(image,3)
+cv2.imwrite(file[:-4]+"_Edges.jpg",edges)
+cv2.imwrite(file[:-4]+"_No_sticks.jpg",no_sticks)
+cv2.imwrite(file[:-4]+"_Sticks.jpg",sticks)
+cv2.imwrite(file[:-4]+"_Orientation.jpg",orientation)
+cv2.imwrite(file[:-4]+"_magnitude.jpg",magnitude)
